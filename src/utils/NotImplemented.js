1360d86795698407532d2451bf45bc4860b9f681
Arthur Little <1690572+littleaj@users.noreply.github.com>
2025-07-19 20:09:29 -0400

    implement NotImplementedError

---
+++ src/utils/NotImplemented.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+
+
+class NotImplementedError extends Error {
+  constructor(message = "This method is not implemented") {
+    super(message);
+    this.name = "NotImplementedError";
+  }
+}
