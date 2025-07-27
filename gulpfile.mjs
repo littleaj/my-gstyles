98812ea885da9e7c4cbfb6ffc7fa09da055f0d50
Arthur Little <1690572+littleaj@users.noreply.github.com>
2025-07-27 11:30:00 -0400

    refactor build task to handle manifest separately. removes '$schema' from manifest before push. excludes manifest from copySource since it's handled separately.

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,16 +1,32 @@
 import { src, dest, series } from "gulp";
 import { deleteAsync as del } from "del";
+import path from "path";
+// cspell:disable
 
 
 const sourceDir = "src";
 const outputDir = "project";
+const manifestFilename = "appsscript.json";
 
 export async function clean() {
   del([`${outputDir}/**`, `!${outputDir}`]);
 }
 
-export async function build() {
-  src(`${sourceDir}/**/*`).pipe(dest(outputDir));
+async function transformManifest() {
+  src(path.join(sourceDir, manifestFilename))
+    .on("data", async file => {
+      const json = JSON.parse(file.contents.toString());
+      delete json["$schema"];
+      file.contents = Buffer.from(JSON.stringify(json, null, 2));
+    })
+    .pipe(dest(outputDir));
 }
+
+async function copySource() {
+  src([`${sourceDir}/**/*`, `!${sourceDir}/${manifestFilename}`])
+    .pipe(dest(outputDir));
+}
+
+export const build = series(copySource, transformManifest);
 
 export default series(clean, build);
