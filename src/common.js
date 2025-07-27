cd8100fd6962f0d7f56f333a29c2cb04bb3d187c
Arthur Little <1690572+littleaj@users.noreply.github.com>
2025-07-27 08:44:16 -0400

    add debug logging to lifecycle methods

--- src/common.js
+++ src/common.js
@@ -7,9 +7,27 @@
 
 
 
+/**
+ * @param {GoogleAppsScript.Events.AddonOnInstall} e
+ */
 function onInstall(e) {
   // TODO initialize styles storage
   // TODO add example styles: code, blockquote
+  console.log("Styles installed. ", e);
+  buildAddOnMenu(e.authMode);
+}
+
+/**
+ * @param {ScriptApp.AuthMode}
+ */
+function buildAddOnMenu(authMode) {
+  SpreadsheetApp.getUi().createAddonMenu()
+    .addItem("Hello", "doHello")
+    .addToUi();
+}
+
+function doHello() {
+  console.log("Hello from My-gStyles!");
 }
 
 /**
@@ -17,5 +35,6 @@ function onInstall(e) {
  * @returns {GoogleAppsScript.Card_Service.Card[]}
  */
 function onCommonHomepage(e) {
+  console.log("Styles homepage opened.");
   return [getStylesCard()];
 }
