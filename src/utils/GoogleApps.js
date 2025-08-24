d2664556db067ca070a2ded3fe9a4cf36fa7cc3a
Arthur Little <1690572+littleaj@users.noreply.github.com>
2025-08-24 09:48:09 -0400

    add utility for accessing current app resources, e.g. Ui, etc.

---
+++ src/utils/GoogleApps.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+
+
+
+const GoogleApps = (function() {
+
+  //#region private functions
+  function getUi() {
+    const allUis = {
+      Sheets: () => SpreadsheetApp.getUi(),
+      Docs: () => DocumentApp.getUi(),
+      Slides: () => SlidesApp.getUi(),
+    }
+
+    let foundUi = null;
+    const name = Object.keys(allUis)
+      .find((name) => {
+        try {
+          foundUi = allUis[name]();
+          return true;
+        } catch {
+          return false;
+        }
+      });
+
+    console.log("Initialized Google App Ui");
+    return new Proxy(foundUi, {
+      get(target, prop, receiver) {
+        if (prop === "name") {
+          return name;
+        }
+        return Reflect.get(target, prop, receiver);
+      }
+    });
+  }
+  //#endregion private functions
+
+  const Ui = getUi();
+  return {
+    Ui,
+  }
+})();
